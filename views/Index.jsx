2566f47b9c4e16c84de9314ab0f01d78771b3a6e
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-14 10:58:48 -0400

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,6 +1,11 @@
 const React = require("react");
 
 class Index extends React.Component {
+
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
   render() {
     const myStyle = {
       color: '#ffffff',
@@ -10,6 +15,15 @@ class Index extends React.Component {
     return (
           <div style={myStyle}>
             <h1>See All The Pokemon!</h1>
+            <ul>
+              {this.props.pokemon.map((poke, index) => (
+                <li key={index}>
+                  <a href={`/pokemonroute/${index}`}>
+                    {this.capitalizeFirstLetter(poke.name)}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
     )
   }
